19a7854bc41fe0aec728b83d8d021c3c35f19d6e
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-03-06 17:05:55 -0300

    Add project files.

---
+++ Calculadora.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Calculadora.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora Tabajara 2025");
+
+            Console.WriteLine("Digite o primeiro número: ");
+            string primeiroNumeroString = Console.ReadLine();
+            int primeiroNumero = Convert.ToInt32(primeiroNumeroString);
+            
+            Console.WriteLine("Digite o segundo número: ");
+            string segundoNumeroString = Console.ReadLine();
+            int segundoNumero = Convert.ToInt32(segundoNumeroString);
+
+
+            int resultado = primeiroNumero + segundoNumero;
+
+            Console.WriteLine(resultado);
+
+            Console.ReadLine();
+
+        }
+    }
+}
